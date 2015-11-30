b0ec258441e4e06f66ab8697c6dcdded5aedc3ab
Avia Efrat <avia.efrat@gigaspaces.com>
2015-11-30 17:42:02 +0200

    Update setup.py
    - Add entry_points
    - Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from setuptools import setup
 
 setup(
     name='tattle',
-    version='0.1.0',
+    version='0.1.2',
     packages=[
         'tattle',
     ],
     url='https://github.com/cloudify-cosmo/tattle',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
+    entry_points={
+        'console_scripts': [
+            'tattle = tattle.engine:main',
+        ]
+    },
     install_requires=[
-        'requests>=2.7.0',
+        'requests>=2.5.4.1',
         'pyyaml>=3.11'
     ],
 )
